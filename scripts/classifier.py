4d806f4b0d1f4c6ad333039fd4fab488582d30bf
dawnchandler <dawnstellachandler@gmail.com>
2019-12-07 00:36:57 -0800

    Cleaned up classifier.

--- scripts/classifier.py
+++ scripts/classifier.py
@@ -13,11 +13,9 @@ from sklearn.metrics import accuracy_score
 cols = ['comment_author', 'num_chars', 'has_digits', 'num_tokens', 'has_first_name', 'has_last_name', 'num_words']
 path_to_data = './data/output.csv'
 usernames_data = pd.read_csv(path_to_data)
-#print(usernames_data)
 
 path_to_target = './data/comments_1.5k.target'
 usernames_target = pd.read_csv(path_to_target)
-#print(usernames_target)
 
 # Convert data to numeric types:
 # Scikit can't handle strings. I learned that the hard way.
@@ -29,7 +27,6 @@ le = preprocessing.LabelEncoder()
 str_cols = ['comment_author', 'has_digits', 'has_first_name', 'has_last_name'] # Includes string and Boolean.
 for col in str_cols:
     usernames_data[col] = le.fit_transform(usernames_data[col])
-#print(usernames_data)
 # Target:
 #print("is_anonymous: ",usernames_target['is_anonymous'].unique()) # Strings.
 str_cols = ['is_anonymous'] # Includes string and Boolean.
@@ -37,19 +34,13 @@ for col in str_cols:
     usernames_target[col] = le.fit_transform(usernames_target[col])
 #print(usernames_target.head(n=25))
 
-
 # Train/Test split:
 # TODO: Create config.
 cols = [col for col in usernames_data.columns if col not in ['comment_author']]
 data = usernames_data[cols]
 target = usernames_target
-#print(data)
-#print(target)
 # Split data set into train and test sets:
 data_train, data_test, target_train, target_test = train_test_split(data, target, test_size = 0.20, random_state = 10)
-
-
-
 
 # Training:
 svc_model = LinearSVC(random_state=0)
@@ -59,50 +50,7 @@ pred = svc_model.fit(data_train, target_train).predict(data_test)
 print('LinearSVC accuracy : ', accuracy_score(target_test, pred, normalize = True))
 
 #print("target_test: ", target_test['is_anonymous'])
-print(type(target_test)) # DataFrame.
-print(type(pred)) # Numpy array.
+#print(type(target_test)) # DataFrame.
+#print(type(pred)) # Numpy array.
 target_test.to_csv('./data/target_test.csv', encoding='utf-8')
 pd.DataFrame(pred).to_csv('./data/pred.csv', encoding='utf-8')
-
-quit()
-
-
-
-
-
-
-#clf = svm.SVC()
-#clf.fit(usernames_data, usernames_target)
-'''
-# Option A:
-# Setup SVC with certain parameters.
-clf = svm.SVC(gamma=0.001, C=100.)
-# Train on all data but last one.
-clf.fit(iris.data[:-1], iris.target[:-1])
-'''
-'''
-# Option B:
-clf = svm.SVC()
-#X, y = datasets.load_iris(return_X_y=True)
-#clf.fit(X, y)
-'''
-
-# Save model:
-'''
-import pickle
-s = pickle.dumps(clf)
-clf2 = pickle.loads(s)
-'''
-
-# Check prediction against labelled data:
-#pred = clf.predict(usernames_data[-1:])
-#print(pred)
-'''Option A:
-pred = clf.predict(iris.data[-1:])
-print(pred)
-''' 
-'''
-# Option B:
-print(clf2.predict(X[0:1]))
-print(y[0])
-'''
